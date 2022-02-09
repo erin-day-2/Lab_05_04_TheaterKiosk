d4cc53262451de3ab1419133afee4ba1d9574714
erin-day-2 <daye4@mail.uc.edu>
2022-02-09 11:25:01 -0500

    finished

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
     public static void main(String[] args)
     {
 	boolean twentyOne = false;
-    int age = 21;
+    int age = 19;
 
     System.out.println("What is your age?");
 
