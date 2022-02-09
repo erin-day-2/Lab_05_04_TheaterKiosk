b362f65364d2378ae52d61cad6b2c6b8e7fc65d5
erin-day-2 <daye4@mail.uc.edu>
2022-02-09 11:24:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	boolean twentyOne = false;
+    int age = 21;
+
+    System.out.println("What is your age?");
+
+    if (age >= 21)
+    {
+        System.out.println("You get a wrist band!");
+    }
+    }
+}
